c0d1f8fdeb83b33fdb11e7d3e4a42f6efcfcd3b4
Caio Schmidt <caiofonseca6@gmail.com>
2023-08-23 08:11:28 -0300

    Adicionar conexao ao BD e rota de cadastro

--- WebApplication1/WebApplication1/Models/UsuarioSeguidorModel.cs
+++ WebApplication1/WebApplication1/Models/UsuarioSeguidorModel.cs
@@ -1,12 +1,17 @@
-﻿using Postgrest.Models;
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
 
 namespace WebApplication1.Models;
 
+[Table("USUARIO_SEGUIDOR")]
 public class UsuarioSeguidorModel : BaseModel
 {
+    [PrimaryKey("ID_USUARIO_SEGUIDOR", false)]
     public long IdUsuarioSeguidor { get; set; }
     
+    [Column("ID_USUARIO")]
     public long IdUsuario { get; set; }
     
+    [Column("ID_SEGUIDOR")]
     public long IdSeguidor { get; set; }
 }
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,6 @@
 using Supabase;
+using WebApplication1.Contracts;
+using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +30,54 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapPost("/cadastro", async (
+    CreateUsuarioRequest request,
+    Supabase.Client client) =>
+    {
+        var usuario = new UsuarioModel
+        {
+            Nome = request.Nome,
+            Descricao = request.Descricao,
+            Senha = request.Senha,
+            Email = request.Email,
+            FotoPerfil = request.FotoPerfil
+        };
+
+        var response = await client.From<UsuarioModel>().Insert(usuario);
+
+        var novoUsuario = response.Models.First();
+
+        return Results.Ok(novoUsuario.IdUsuario);
+    });
+
+app.MapGet("/usuarios/{id}", async (long IdUsuario, Supabase.Client client) =>
+    {
+        var response = await client
+            .From<UsuarioModel>()
+            .Where(n => n.IdUsuario == IdUsuario)
+            .Get();
+
+        var usuario = response.Models.FirstOrDefault();
+
+        if (usuario is null)
+        {
+            return Results.NotFound();
+        }
+
+        var usuarioResponse = new UsuarioResponse
+        {
+            IdUsuario = usuario.IdUsuario,
+            Nome = usuario.Nome,
+            //Senha = usuario.Senha
+            Descricao = usuario.Descricao,
+            //Email = usuario.Email
+            FotoPerfil = usuario.FotoPerfil,
+            DataCriacao = usuario.DataCriacao
+        };
+
+        return Results.Ok(usuarioResponse);
+    });
 
 app.UseHttpsRedirection();
 
